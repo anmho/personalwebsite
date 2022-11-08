a5995684e58ec9942440c10288ccf472b40b4098
Andrew Ho <andyminhtuanho@gmail.com>
2022-11-07 19:17:31 -0800

    Updated resume so it opens default pdf handler instead of downloading the file

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,9 +6,12 @@ function Resume({ parent }) {
 
   return (
     <div className="shadow-xl hover:shadow-2xl w-max hover:cursor-pointer transition-all duration-300 hover:-translate-y-3 p-1">
-      <a href="https://github.com/anmho/resume/raw/main/main.pdf">
+      <a
+        href="https://personalwebsiteserver.herokuapp.com/resume.pdf"
+        target="_blank"
+      >
         <Document
-          file="https://personalwebsiteserver.herokuapp.com/"
+          file="https://personalwebsiteserver.herokuapp.com/resume.pdf"
           onLoadSuccess={() => {
             // console.log(parent.current.clientHeight, parent.current.clientWidth);
             console.log(window.innerHeight, window.innerWidth);
